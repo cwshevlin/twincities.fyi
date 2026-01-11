0638cb1098f1f2b3d4aeba8e4047a2da98795f4c
Colin Shevlin <cwshevlin@gmail.com>
2026-01-10 18:46:19 -0600

    Adds a test article.

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = 'Colin Shevlin'
+SITENAME = 'twincities.fyi'
+SITEURL = "https://twincities.fyi"
+
+PATH = "content"
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
